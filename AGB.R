d9083f59afbb6dec20c64e7ca46c06c022ba1b7c
fsbmat-ufv <fernando.bastos@ufv.br>
2025-05-02 17:23:19 -0300

    correcoes

--- AGB.R
+++ AGB.R
@@ -12,43 +12,52 @@ df <- read.csv("ess/data/ForestGEO.csv",
 # Pre-processamento dos dados
 df <- df %>%
   mutate(
-    DAP                 = suppressWarnings(as.numeric(as.character(dbh.full.census)))/10,
-    DENS                = as.numeric(meanWD),
-    AltDAP              = as.numeric(hom),
-    AltCDano            = as.numeric(H_considering_damage),
-    Prop                = as.numeric(weights.ind),
-    Local               = factor(site),
+    dbh.full.census     = suppressWarnings(as.numeric(as.character(dbh.full.census)))/10,#Transformar DAP em cm
+    meanWD              = as.numeric(meanWD),
+    hom                 = as.numeric(hom),
+    H_considering_damage= as.numeric(H_considering_damage),
+    weights.ind         = as.numeric(weights.ind),
+    site                = factor(site),
     sel                 = as.integer(status == "A"),
     date.ams            = as.Date(date.ams),
     date.full.census    = as.Date(date.full.census),
-    time_since_census   = as.numeric(date.ams - date.full.census)
+    time_since_census   = as.numeric(date.ams - date.full.census),
+    b                   = as.numeric(b)/100,
+    b                   = ifelse(is.na(b), 0, b),
+    b                   = ifelse(sel == 0, 0, b),
+    H_considering_damage= ifelse(sel, H_considering_damage, 0),
+    logH = ifelse(sel, log(H_considering_damage), 0)
   ) %>%
   filter(
     !is.na(sel),
-    !is.na(DAP),
-    !is.na(DENS),
-    !is.na(AltDAP),
-    !is.na(Local),
+    !is.na(dbh.full.census),
+    !is.na(H_considering_damage),
+    !is.na(meanWD),
+    !is.na(hom),
+    !is.na(site),
     !is.na(time_since_census),
-    !is.na(Prop),
-    is.finite(DAP),
-    is.finite(Prop),
+    !is.na(weights.ind),
+    is.finite(dbh.full.census),
+    is.finite(weights.ind),
     is.finite(time_since_census)
   ) %>%
   droplevels()
 
-#names(df) <- c("Local", 
-#               "IDMort", 
-#               "ID", 
-#               "DataCenso", 
-#               "DAP", 
-#               "AltDAP", 
-#               "DENS", 
-#               "DataPesq",
-#               "Status",
-#               "Alt",
-#               "PropVol",
-#               "Freq")
+names(df) <- c("Local",
+              "IDMort",
+              "ID",
+              "dateCensus",
+              "DAP",
+              "AltDAP",
+              "DENS",
+              "dateAms",
+              "Status",
+              "AltCDano",
+              "PropVol",
+              "Prop",
+              "sel", 
+              "time_since_census",
+              "logH")
 #head(df)
 #df$DAP <- df$DAP/10
 # Definir coordenadas dos locais
@@ -68,7 +77,6 @@ coord <- cbind(coord_local$long, coord_local$lat)
 IndexE <- readRDS("IndexE.Rds")
 # Juntar o índice E no seu dataframe
 df <- merge(df, IndexE, by = "Local")
-
 # Aplicar a fórmula de altura diretamente:
 df$H1 <- exp(0.893 - df$E + 0.760 * log(df$DAP) - 0.0340 * (log(df$DAP))^2)
 
@@ -92,70 +100,44 @@ df$H1[df$Local == "fushan"    & df$H1 > 23] <- 23
 #                 0.0461*((log(df$DAP))^2))
 # Fórmula de Chave et al. (2014) com DAP, H e densidade
 #computeAGB retorna medidas em Mg: 1 megagrama (Mg) = 1.000 kg
-df$AGB <- (1000*computeAGB(df$DAP, df$DENS, H=df$H1))*df$Prop
-df$AGB2 <- (0.0673 * (df$DENS * (df$DAP^2) * df$H1)^0.976)*df$Prop
+df$AGB <- (1000*computeAGB(df$DAP, df$DENS, H=df$AltCDano))*df$Prop #Valor em Toneladas
+#df$AGB2 <- (0.0673 * (df$DENS * (df$DAP^2) * df$AltCDano)^0.976)*df$Prop #Valor em Toneladas
 
+head(df)
+summary(df$DAP) # DAP em cm
+summary(df$DENS) # DENS em g/cm^3
+summary(df$AltCDano) # AltCDano em metros
 summary(df$AGB)
-summary(df$AGB2)
-summary(df$DAP)
-hist(df$Prop)
+#summary(df$AGB2)
 
-head(df[df$AGB > 10000, c("DAP", "H1", "DENS", "AGB", "AGB2")])
+
+head(df[df$AGB > 1, c("DAP", "H1", "DENS", "AGB")])
 # Histograma da AGB2
-hist(df$AGB, breaks = 100, main = "Distribuição da Biomassa (AGB2)", 
-     xlab = "AGB (kg)", col = "lightblue", xlim = c(0, quantile(df$AGB2, 0.99, na.rm = TRUE)))
-
-# Resumo estatístico detalhado
-summary(df$AGB)
-
-# Estatísticas adicionais
-cat("Média:", mean(df$AGB, na.rm = TRUE), "\n")
-cat("Mediana:", median(df$AGB, na.rm = TRUE), "\n")
-cat("Máximo:", max(df$AGB, na.rm = TRUE), "\n")
-cat("Mínimo:", min(df$AGB, na.rm = TRUE), "\n")
-cat("Desvio padrão:", sd(df$AGB, na.rm = TRUE), "\n")
+hist(df$AGB, breaks = 100, main = "Distribuição da Biomassa (AGB)", 
+     xlab = "AGB (Toneladas)", col = "lightblue", xlim = c(0, quantile(df$AGB, 0.99, na.rm = TRUE)))
 
 # Quantos indivíduos tem biomassa acima de 1000 kg (1 tonelada)
-cat("Número de árvores com AGB > 1000 kg:", sum(df$AGB > 1000, na.rm = TRUE), "\n")
+cat("Número de árvores com AGB > 1 kg:", sum(df$AGB > 1, na.rm = TRUE), "\n")
 
 # Listar as 10 árvores com maior AGB
 top_agb <- df[order(-df$AGB), c("DAP", "H1", "DENS", "AGB")]
 head(top_agb, 10)
 
-# Marcar gigantes (opcional)
-df$Gigante <- ifelse(df$AGB > 10000, "Sim", "Não")
-
-# Quantos gigantes
-table(df$Gigante)
-
-subset(df, weights.ind > 0.1)
-head(df)
-
-
 
 # Criar logH somente onde H_considering_damage está disponível (ou zero se for censurado)
 df <- df %>%
   mutate(
-    AltCDano = ifelse(sel, AltCDano, 0),
-    logH = log1p(AltCDano),
-    AGB = ifelse(sel, AGB, 0),
-    logAGB = log1p(AGB),
-    b = as.numeric(b),
-    b = ifelse(is.na(b) & sel == 0, 0, b)
-  ) %>%
-  filter(!is.na(b), is.finite(b))
+    logAGB = ifelse(sel == 1 & is.finite(AGB) & AGB > 0, log(AGB), NA),
+    logDAP               = log(DAP), 
+    logDAP_z             = scale(log(DAP)),
+    DENS_z               = scale(DENS),
+    AltDAP_z             = scale(AltDAP),
+    time_since_census_z  = scale(time_since_census),
+    b_z                  = scale(PropVol))
 
-df <- df %>% mutate(b = b / 100,
-                    log_DAP              = scale(log(DAP)),
-                    DENS                 = scale(DENS),
-                    AltDAP                = scale(AltDAP),
-                    time_since_census_z  = scale(time_since_census),
-                    b_z                  = scale(b)
-)
+selectEq  <- sel ~ logDAP_z + DENS_z + time_since_census_z
+outcomeEq <- logAGB ~ logDAP_z + DENS_z + b_z + AltCDano + Local
 
-
-selectEq  <- sel ~ log_DAP + DENS + time_since_census_z
-outcomeEq <- logAGB ~ log_DAP + b_z + Local
 modelo_heckit <- selection(
   selection = selectEq,
   outcome   = outcomeEq,
@@ -166,12 +148,51 @@ start_manual <- coef(modelo_heckit, part = "full")
 start_manual <- unname(start_manual)  # remove nomes para o ssmodels
 
 
+
+# Criar logH somente onde H_considering_damage está disponível (ou zero se for censurado)
+df2 <- df %>%
+  mutate(
+    logAGB = ifelse(sel == 1 & is.finite(AGB) & AGB > 0, log(AGB), 0))
+
+modeloCL <- HeckmanCL(
+  selection = selectEq,
+  outcome   = outcomeEq,
+  data      = df2,
+  start = start_manual)
+summary(modeloCL)
+
+
 start_manual <- c(start_manual, 5) 
 
 modelo_ts <- HeckmantS(
   selection = selectEq,
   outcome   = outcomeEq,
-  data      = df,
-  df        = 5,
-  start     = start_manual)
+  data      = df2,
+  start = start_manual)
 summary(modelo_ts)
+
+# Criar logH somente onde H_considering_damage está disponível (ou zero se for censurado)
+df2 <- df %>%
+  mutate(
+    logAGB = ifelse(sel == 1 & is.finite(AGB) & AGB > 0, log(AGB), NA),
+    AGB2   = ifelse(sel == 1 & is.finite(AGB) & AGB > 0, AGB, 0))
+
+selectEq  <- sel ~ logDAP_z + DENS_z + time_since_census_z
+outcomeEq <- AGB2 ~ logDAP_z + DENS_z + b_z + AltCDano + Local
+
+outcomeCL <- logAGB ~ logDAP_z + DENS_z + b_z + AltCDano + Local
+
+modelo_heckit <- selection(
+  selection = selectEq,
+  outcome   = outcomeCL,
+  data      = df2)
+summary(modelo_heckit)
+
+start_manual <- coef(modelo_heckit, part = "full")
+start_bs <- c(unname(start_manual), 1)  # adiciona phi1 = 1
+
+modelo_bs <- HeckmanBS(
+  selection = selectEq,
+  outcome   = outcomeEq,
+  data      = df2,
+  start = start_bs)
